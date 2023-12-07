9236016313d30aac610ffd07dcbe7a87b8480fd4
valeriaaa12 <valeriaromero1275@gmail.com>
2023-12-07 17:10:57 -0600

    ya imprime

--- Figuras_Planas.cpp
+++ Figuras_Planas.cpp
@@ -2,11 +2,11 @@
 #include "Paralelogramo.h"
 #include "Cuadrado.h"
 #include <windows.h>
-
+#include "Rectangulo.h"
 using namespace std;
 int main(int argc, char* argv[]) {
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
-    Paralelogramo p(1, 2, 3);
-    p.Dibujar();
+    Rectangulo r( 10, 10);
+    r.Dibujar();
 }
